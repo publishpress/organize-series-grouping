58c7423f68f606857c846114d719ddd2f7199d21
Darren Ethier <darren@roughsmootheng.in>
2012-06-18 11:09:16 -0400

    [completed: 15]

--- organize-series-grouping.php
+++ organize-series-grouping.php
@@ -11,7 +11,7 @@ $orgseries_groups_ver = '2.2';
 global $orgseries_groups_ver;
 
 /* LICENSE */
-//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2011 Darren Ethier. This program is free software; you can redistribute it and/or
+//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2012 Darren Ethier. This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
