74d75389211cf86d81966568a48478392dade263
Darren Ethier <darren@roughsmootheng.in>
2017-10-08 15:45:17 -0400

    integrate with the new core licensing and bootstrapping processes #11

---
+++ src/domain/Meta.php
@@ -0,0 +1,9 @@
+<?php
+namespace OrganizeSeries\GroupingAddon\domain;
+
+use OrganizeSeries\domain\interfaces\AbstractMeta;
+
+class Meta extends AbstractMeta
+{
+    //Used for any grouping add-on specific paths etc.
+}
